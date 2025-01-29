c3f887b0bf093f92b8ff9c335175ffc754f90f35
黒川颯 <ryu@kurokawasatsunoMacBook-Pro.local>
2025-01-29 19:40:42 +0900

    Googleでのログイン機能実装

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,22 @@
+import { signInWithPopup } from 'firebase/auth';
 import React from 'react';
+import { auth, provider } from '../firebase';
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ setIsAuth }) => {
+  const loginInWithGoogle = () => {
+    //Googleでログイン
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem('isAuth,true');
+
+      setIsAuth(true);
+    });
+  };
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
+  );
 };
 
 export default Login;
